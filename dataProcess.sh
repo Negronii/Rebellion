34ed0755622ccdb1cdcef971c82cac682e8e72a6
Zixuan Liu <zixliu@student.unimelb.edu.au>
2022-05-22 17:42:59 +1000

    read experiment output

--- dataProcess.sh
+++ dataProcess.sh
@@ -1,3 +1,3 @@
-rm -r processedData
-mkdir processedData
-python dataProcessing/processor.py -i inputData/default.csv -n default
+# rm -r processedData
+# mkdir processedData
+python dataProcessing/processor.py -i inputData/default.csv
