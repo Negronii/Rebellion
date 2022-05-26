9230ad53f0bd012c7343889f29694ad799f3f3fc
Zixuan Liu <zixliu@student.unimelb.edu.au>
2022-05-26 19:38:26 +1000

    first image generated

---
+++ dataAnalysis/extensionAnalysis.py
@@ -0,0 +1,14 @@
+import sys, csv, math
+import matplotlib.pyplot as plt
+
+with open('dataSamples/extension/result.csv', 'r') as openfile:
+    reader = csv.reader(openfile)
+    data = list(reader)
+data = data[1:]
+x = []
+y = []
+for i in range(len(data)):
+    x.append(float(data[i][0])/100)
+    y.append(float(data[i][1]))
+plt.scatter(x, y, color="red", alpha=0.5, s=20)
+plt.show()
