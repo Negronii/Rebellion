c109fc6101de0b9154ebe1aa8586dc10c2c614aa
Zixuan Liu <zixliu@student.unimelb.edu.au>
2022-05-26 20:35:04 +1000

    changed data

--- dataAnalysis/extensionAnalysis.py
+++ dataAnalysis/extensionAnalysis.py
@@ -8,7 +8,7 @@ data = data[1:]
 x = []
 y = []
 for i in range(len(data)):
-    x.append(float(data[i][0])/100)
+    x.append(float(data[i][0]))
     y.append(float(data[i][1]))
 plt.scatter(x, y, color="red", alpha=0.5, s=20)
 plt.show()
